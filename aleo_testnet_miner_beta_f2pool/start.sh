1159e19fa070ab6c49ade486bea15544ed2f8846
harleyLuke <carlenata2436918@gmail.com>
2024-07-02 15:13:07 +0800

    aleo_testnet_miner_beta_f2pool

--- aleo_testnet_miner_beta_f2pool/start.sh
+++ aleo_testnet_miner_beta_f2pool/start.sh
@@ -2,5 +2,12 @@
 cd /home/docker/data
 random_number=$((RANDOM *RANDOM))
 export WORKERNAME=$random_number
-nohup ./aleo-miner -u $MINER_URL -w $ACCOUNTNAME.$WORKERNAME -d 0 >> ./aleo-miner.log 2>&1 &
+enableGPU=$enableGPU
+if [ $enableGPU -eq 1 ]; then
+    nohup ./aleo-miner -u $MINER_URL -w $ACCOUNTNAME.$WORKERNAME -d 0 >> ./aleo-miner.log 2>&1 &
+    echo "nohup ./aleo-miner -u $MINER_URL -w $ACCOUNTNAME.$WORKERNAME -d 0 >> ./aleo-miner.log 2>&1 &" > ./aleo-miner.log
+else
+    nohup ./aleo-miner -u $MINER_URL -w $ACCOUNTNAME.$WORKERNAME  >> ./aleo-miner.log 2>&1 &
+    echo "nohup ./aleo-miner -u $MINER_URL -w $ACCOUNTNAME.$WORKERNAME  >> ./aleo-miner.log 2>&1 &" > ./aleo-miner.log
+fi
 streamlit run run.py
