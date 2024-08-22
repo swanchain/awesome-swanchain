20bc549fed36ca3cd4ef578b58f0f8ec9943604a
Pluto Zhang <pluto.zhang@nbai.io>
2024-08-22 18:38:07 -0400

    Modify

--- get-balance-on-filecoin/Readme.md
+++ get-balance-on-filecoin/Readme.md
@@ -1,25 +1,28 @@
-## Get Started 
+.
+
+## Get Started
 
 If you want to run a "hello world" docker project, please follow the steps below
 
 ## Build  Image
 
-
 ###
+
 ```shell
 cd hello_world
 docker build -t lad_hello_world .
 ```
+
 ## Start a instance
 
 ```shell
- docker run lad_hello_world
+ docker run -p 7860:7860 lad_hello_world 
 ```
 
-Open the web page at http://0.0.0.0:7860, if success you can the following response 
+Open the web page at http://0.0.0.0:7860, if success you can the following response
 
 ```json
 {
-    "Hello": "World!"
+  "Hello": "World!"
 }
 ```
---
+++ serverless-api/app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+database = []
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+@app.post("/items/", response_model=Item)
+def create_item(item: Item):
+    database.append(item)
+    return item
+
+@app.get("/items/", response_model=List[Item])
+def read_items():
+    return database
+
+@app.get("/items/{item_id}", response_model=Item)
+def read_item(item_id: int):
+    for item in database:
+        if item.id == item_id:
+            return item
+    return {"error": "Item not found"}
